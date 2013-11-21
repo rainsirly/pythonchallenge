0a1313632c096569a4f67a96f405ea43890316ed
Rainsirly <leiyu8948@gmail.com>
2013-11-20 21:31:33 -0800

    modify No.4 to fit it well

--- 4_.py
+++ 4_.py
@@ -5,14 +5,14 @@
 import urllib
 import re
 
-url_start = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345"
+url_start = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=8022"
 #the num(12345) will changed while the web visit running, and themilestone is 16044(need to
 #divide it by two and keep going)
-check = re.compile(r'[0-9]+')
-url_num = "".join(check.findall(url_start))
+check = re.compile(r'([0-9]+)')
+url_num = check.findall(url_start)[-1]
 url_head = url_start.rsplit(url_num)[0]
 while True:
-    url = url_head + url_num
+    url = "%s%s" % (url_head, url_num)
     try:
         page = urllib.urlopen(url)
         print "visit page %s" % url
@@ -20,7 +20,8 @@ while True:
         print "cannt open page %s" % url
         break
     text = page.read()
-    url_num = "".join(check.findall(text))
-    if not url_num:
+    try:
+        url_num = check.findall(text)[-1]
+    except:
         print text
         break
